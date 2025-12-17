bd3cfcabbf360d3f7fe004ade1014f0df5067e30
SATYAM KUMAR <139258006+Satyaamp@users.noreply.github.com>
2025-12-17 20:41:26 +0530

    Refactor auth controller to handle errors

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,13 +1,32 @@
 const authService = require('../services/auth.service');
 const { success } = require('../utils/response.util');
 
-exports.register = async (req, res) =>
-  success(res, await authService.register(req.body));
+exports.register = async (req, res, next) => {
+  try {
+    const user = await authService.register(req.body);
+    success(res, user, 'Registration successful');
+  } catch (err) {
+    next(err);
+  }
+};
 
-exports.login = async (req, res) =>
-  success(res, await authService.login(req.body.email, req.body.password));
+exports.login = async (req, res, next) => {
+  try {
+    const user = await authService.login(
+      req.body.email,
+      req.body.password
+    );
+    success(res, user, 'Login successful');
+  } catch (err) {
+    next(err); // ✅ VERY IMPORTANT
+  }
+};
 
-exports.me = async (req, res) => {
-  const user = await authService.getMe(req.user.id);
-  success(res, user, 'User profile fetched');
+exports.me = async (req, res, next) => {
+  try {
+    const user = await authService.getMe(req.user.id);
+    success(res, user, 'User profile fetched');
+  } catch (err) {
+    next(err);
+  }
 };
